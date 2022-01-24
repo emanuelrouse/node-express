c8cf3aab016e0b8e50b7223085e640eb368ffdef
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-01-24 18:42:08 -0500

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('dev')); // morgan handles logging the request info
 
 app.use(express.static(__dirname + '/public')); // dirname: refers the the absolute path of the current directory of the file it's in
 
-app.use((req, res) => {
+app.use(function (req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
