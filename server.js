c843f6fb2e68d40f95f965cd716f33ebc608876d
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-01-23 22:25:20 -0500

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express'); // import express
+const morgan = require('morgan'); // import morgan
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); // return express server application
+app.use(morgan('dev')); // morgan handles logging the request info
+
+app.use(express.static(__dirname + '/public')); // dirname: refers the the absolute path of the current directory of the file it's in
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
